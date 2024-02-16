4a7da1769e055c4edcb16c8e71ff219a7fe26d5a
Manish Shah <manish.shah@krishtechnolabs.local>
2024-02-16 11:53:12 +0530

    pdp page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductDetailPage from './ProductDetailPage';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <ProductDetailPage />
+    </div>
+  );
+}
+
+export default App;
